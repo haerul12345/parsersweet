7c4fffc544b4130b4c28f365c4d3363805fa931e
haerul12345 <haerul.jusmar@hotmail.com>
2025-09-06 13:38:09 +0700

    update grid

--- script.js
+++ script.js
@@ -1577,19 +1577,24 @@ function parseTLV(hex) {
         ];
       }
 
-      let tooltipHtml = `<div style="font-family:monospace;font-size:12px;"><strong>Byte 1 (${byte1}):</strong> 
-      <div style="height: 8px;"></div>
+      let tooltipHtml = `<div style="font-family:monospace;font-size:12px;">`;
+      
+      tooltipHtml += `<strong>Byte 1 (${byte1}):</strong> 
+            <div style="height: 8px;"></div>
            <div style="display: grid; grid-template-columns: auto 1fr; gap: 6px; margin-bottom: 8px;">`;
       for (let k = 0; k < bin1.length; k++) {
-        tooltipHtml += `<input type="checkbox" disabled ${bin1[k] === "1" ? "checked" : ""} style="accent-color: black;>
+        tooltipHtml += 
+        `<input type="checkbox" disabled ${bin1[k] === "1" ? "checked" : ""} style="accent-color: black;>
         <label style="color: black; font-size:12px;>${bitLabels1[k]}</label>`;
       }
       tooltipHtml += `</div>`;
+
       tooltipHtml += `<strong>Byte 2 (${byte2}):</strong>
-          <div style="height: 8px;"></div>
+        <div style="height: 8px;"></div>
         <div style="display: grid; grid-template-columns: auto 1fr; gap: 6px;">`;
       for (let k = 0; k < bin2.length; k++) {
-        tooltipHtml += `<input type="checkbox" disabled ${bin2[k] === "1" ? "checked" : ""}style="accent-color: black;>
+        tooltipHtml += 
+        `<input type="checkbox" disabled ${bin2[k] === "1" ? "checked" : ""}style="accent-color: black;>
         <label style="color: black; font-size:12px;>${bitLabels2[k]}</label></div>`;
       }
       tooltipHtml += `</div></div>`;
